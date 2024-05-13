2b6dbd7083b971bc90e3e4ef9edc4db341dc9d7d
ivanmejias <imejiasmu.inf@upsa.es>
2024-05-13 08:54:10 +0200

    Creación Tablas

---
+++ Biblioteca/TrabajoFinal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrabajofinalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TrabajoFinal'
---
+++ Biblioteca/TrabajoFinal/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Biblioteca(models.Model):
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200)
+    horario_apertura = models.TimeField()
+    horario_cierre = models.TimeField()
+    fecha_fundacion = models.DateField()
+    normas = models.FileField(upload_to='normas/', null=True, blank=True)
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    sinopsis = models.TextField()
+    ano_publicacion = models.DateField()
+    editorial = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    numero_ejemplares = models.IntegerField()
+    biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE)
