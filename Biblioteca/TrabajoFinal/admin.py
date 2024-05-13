f033b3a05b3b379e57f07aaa7a4f75e33afd2f68
ivanmejias <imejiasmu.inf@upsa.es>
2024-05-13 09:04:51 +0200

    Gestion Bibliotecas y Libros

--- Biblioteca/TrabajoFinal/admin.py
+++ Biblioteca/TrabajoFinal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Biblioteca, Libro
+
+admin.site.register(Biblioteca)
+admin.site.register(Libro)
